92260bd6a3e4bd3d0dc5fd1562f724f3e72d16bc
Prajwal <103810119+Prajwal0225@users.noreply.github.com>
2022-07-17 19:54:57 +0530

    My Favorite Superhero

---
+++ script.js
@@ -0,0 +1,40 @@
+var BaseUrl = "https://superheroapi.com/api.php/136899909020706"
+
+const btn = document.getElementById('newherrobtn')
+const heroImageDiv = document.getElementById('heroImage')
+const searchbtn = document.getElementById('searchId')
+const searchInput = document.getElementById('SearchInput')
+
+
+const getSupperHero = (id) =>{
+  fetch(`${BaseUrl}/${id}`)
+  .then(response => response.json())
+  .then(json =>{
+    //console.log(json)
+    const name = `<h2>${json.name}</h2>`
+    heroImageDiv.innerHTML = `${name} <img src= "${json.image.url}" heiht=200
+    width=200/>`
+    })
+}
+
+const getSearchSuperHero = (name) =>{
+   fetch(`${BaseUrl}/search/${name}`)
+  .then(response => response.json())
+  .then(json =>{
+    const hero = json.results[0]
+    // console.log(hero)
+    const hero2 = `<h2>${name}</h2>`
+    heroImageDiv.innerHTML = `${hero2} <img src= "${hero.image.url}" heiht=200 width=200/>`
+    })
+}
+
+
+const randomid =()=> {
+  return Math.floor(Math.random() * 731)+1
+}
+
+
+btn.onclick = () => getSupperHero(randomid())
+
+
+searchbtn.onclick = () => getSearchSuperHero(searchInput.value)
