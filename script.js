c2408aa8934999628bba689cadd1d9fd698eb0ac
Samrat <samratapr40@gmail.com>
2023-10-02 12:56:24 +0530

    download func & ui

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 var BaseUrl = "https://superheroapi.com/api.php/136899909020706"
 
-const btn = document.getElementById('newherrobtn')
-const heroImageDiv = document.getElementById('heroImage')
-const searchbtn = document.getElementById('searchId')
-const searchInput = document.getElementById('SearchInput')
+const searchInput = document.getElementById('SearchInput')  // input:search
+const searchbtn = document.getElementById('searchId') // search button
+const btn = document.getElementById('newherrobtn') // random hero btn
+const heroImageDiv = document.getElementById('heroImage') // output image
+const downloadImageBtn = document.getElementById('downloadImg') // download the image
 
 
 const getSupperHero = (id) =>{
   fetch(`${BaseUrl}/${id}`)
   .then(response => response.json())
   .then(json =>{
-    //console.log(json)
     const name = `<h2>${json.name}</h2>`
-    heroImageDiv.innerHTML = `${name} <img src= "${json.image.url}" heiht=200
-    width=200/>`
+    heroImageDiv.innerHTML = `${name} <img src= "${json.image.url}" height=200 width=200/>`
+    
+    console.log(json.image.url)
+      downloadImageBtn.addEventListener('click', () => {
+        const imageUrl = json.image.url;
+        downloadImage(imageUrl);
+      });
+
     })
 }
 
@@ -22,12 +28,27 @@ const getSearchSuperHero = (name) =>{
   .then(response => response.json())
   .then(json =>{
     const hero = json.results[0]
-    // console.log(hero)
+    console.log(hero)
     const hero2 = `<h2>${name}</h2>`
-    heroImageDiv.innerHTML = `${hero2} <img src= "${hero.image.url}" heiht=200 width=200/>`
+    heroImageDiv.innerHTML = `${hero2} <img src= "${hero.image.url}" height=200 width=200/>`
+
+    console.log(hero.image.url)
+      downloadImageBtn.addEventListener('click', () => {
+        const imageUrl = hero.image.url;
+        downloadImage(imageUrl);
+      });
     })
 }
 
+// Download the image function
+function downloadImage(imageUrl) {
+  const aTag = document.createElement('a');
+  aTag.href = imageUrl;
+  aTag.download = imageUrl.split('/').pop();
+  document.body.appendChild(aTag);
+  aTag.click();
+  aTag.remove();
+}
 
 const randomid =()=> {
   return Math.floor(Math.random() * 731)+1
