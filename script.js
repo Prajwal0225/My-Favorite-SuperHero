9ccc1e5db3f81b46bc78fe83456c39607391be3b
pushkar1713 <pushkar1713@gmail.com>
2023-10-05 22:16:09 +0530

    responsive nav-bar

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 var BaseUrl = "https://superheroapi.com/api.php/136899909020706";
 
-
-
-
-
-
-const btn = document.getElementById('newherrobtn')
-const heroImageDiv = document.getElementById('heroImage')
-const searchbtn = document.getElementById('searchId')
-const searchInput = document.getElementById('SearchInput')
+const btn = document.getElementById("newherrobtn");
+const heroImageDiv = document.getElementById("heroImage");
+const searchbtn = document.getElementById("searchId");
+const searchInput = document.getElementById("SearchInput");
 const downloadImageBtn = document.getElementById("downloadImg"); // download the image
 // const toggleButtton = document.querySelector(".toggle-button");
 // const linkContainer = document.querySelector(".links-container");
@@ -19,12 +14,9 @@ const getCurrentYear = () => {
 };
 getCurrentYear();
 
-
 // toggleButtton.addEventListener('click',()=>{
 //   linkContainer.classList.toggle("active");
 // })
-
-
 
 const getSupperHero = (id) => {
   fetch(`${BaseUrl}/${id}`)
@@ -92,3 +84,28 @@ searchbtn.onclick = () => {
   }
 };
 
+(function ($) {
+  // Begin jQuery
+  $(function () {
+    // DOM ready
+    // If a link has a dropdown, add sub menu toggle.
+    $("nav ul li a:not(:only-child)").click(function (e) {
+      $(this).siblings(".nav-dropdown").toggle();
+      // Close one dropdown when selecting another
+      $(".nav-dropdown").not($(this).siblings()).hide();
+      e.stopPropagation();
+    });
+    // Clicking away from dropdown will remove the dropdown class
+    $("html").click(function () {
+      $(".nav-dropdown").hide();
+    });
+    // Toggle open and close nav styles on click
+    $("#nav-toggle").click(function () {
+      $("nav ul").slideToggle();
+    });
+    // Hamburger to X toggle
+    $("#nav-toggle").on("click", function () {
+      this.classList.toggle("active");
+    });
+  }); // end DOM ready
+})(jQuery); // end jQuery
