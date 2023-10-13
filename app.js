37de2660215c7a4eaffb9991acf7bea271df6316
Pavilion-devs <olaboyefavour52@gmail.com>
2023-10-13 03:56:37 +0100

    Displayed contributors data

---
+++ app.js
@@ -0,0 +1,36 @@
+// server.js
+
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+const port = process.env.PORT || 3000;
+const githubToken = process.env.GITHUB_TOKEN; // Set this environment variable
+
+app.use(express.json());
+app.use(express.static(__dirname));
+
+
+app.get('/', (req, res) => {
+    // Serve the index.html file
+    res.sendFile(__dirname + '/index.html');
+  });
+// Define an endpoint that makes requests to the GitHub API using your token
+app.get('/api/github/contributors', async (req, res) => {
+  try {
+    const owner = 'Pavilion-devs';
+    const repo = 'My-Favorite-SuperHero';
+    const response = await axios.get(`https://api.github.com/repos/${owner}/${repo}/contributors`, {
+      headers: {
+        Authorization: `token ${githubToken}`,
+      },
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
