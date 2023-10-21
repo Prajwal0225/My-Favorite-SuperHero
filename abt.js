796ca1615e6fcfa8cbd75638e30e012d9b7d7d8c
Saurav Kumar <krsaurav2208@gmail.com>
2023-10-22 00:16:05 +0530

    Updated Styling curious cases

---
+++ abt.js
@@ -0,0 +1,14 @@
+document.querySelector('#question1').addEventListener('click', function () {
+    document.querySelector('#answer1').classList.toggle('hidden');
+    document.querySelector('#item1').classList.toggle('style');
+});
+
+document.querySelector('#question2').addEventListener('click', function () {
+    document.querySelector('#answer2').classList.toggle('hidden');
+    document.querySelector('#item2').classList.toggle('style');
+});
+
+document.querySelector('#question3').addEventListener('click', function () {
+    document.querySelector('#answer3').classList.toggle('hidden');
+    document.querySelector('#item3').classList.toggle('style');
+});
