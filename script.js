c898871703171ae59f4204fd9bae57e85ad0fd07
Samyak <121035286+Samyaktg@users.noreply.github.com>
2023-10-22 08:25:13 +0000

    Added typewriter effect to searchbar

--- script.js
+++ script.js
@@ -166,3 +166,34 @@ searchInput.addEventListener("keyup", (e) => {
     });
   }); // end DOM ready
 })(jQuery); // end jQuery
+
+
+const staticText = "Search For";
+const dynamicText = document.getElementById("SearchInput");
+const words = ["Superman", "Batman", "Flash", "Iron Man"];
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+const typeEffect = () => {
+    const currentWord = words[wordIndex];
+    const currentChar = currentWord.substring(0, charIndex);
+    const animatedText = staticText + " " + currentChar; // Combine static and animated text
+    dynamicText.setAttribute("placeholder", animatedText);
+    dynamicText.classList.add("stop-blinking");
+
+    if (!isDeleting && charIndex < currentWord.length) {
+        charIndex++;
+        setTimeout(typeEffect, 200);
+    } else if (isDeleting && charIndex > 0) {
+        charIndex--;
+        setTimeout(typeEffect, 100);
+    } else {
+        isDeleting = !isDeleting;
+        dynamicText.classList.remove("stop-blinking");
+        wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+        setTimeout(typeEffect, 1200);
+    }
+}
+
+typeEffect();
