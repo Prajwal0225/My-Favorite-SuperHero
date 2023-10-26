4ced19c5d45af24d8e952495f73bb52fad5a9070
saurav2208 <125905089+saurav2208@users.noreply.github.com>
2023-10-26 12:45:37 +0000

    Updated styling curious cases

--- js/about.js
+++ js/about.js
@@ -1,1 +1,15 @@
 // script for /about.html
+document.querySelector('#question1').addEventListener('click', function () {
+    document.querySelector('#answer1').classList.toggle('hidden');
+    document.querySelector('#item1').classList.toggle('style');
+});
+
+document.querySelector('#question2').addEventListener('click', function () {
+    document.querySelector('#answer2').classList.toggle('hidden');
+    document.querySelector('#item2').classList.toggle('style');
+});
+
+document.querySelector('#question3').addEventListener('click', function () {
+    document.querySelector('#answer3').classList.toggle('hidden');
+    document.querySelector('#item3').classList.toggle('style');
+});
